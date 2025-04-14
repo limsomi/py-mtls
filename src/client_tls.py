b2cc747187a4597317628c0d59776e969e270b05
hurmong@hanmail.net <limsomi79@gmail.com>
2025-04-14 16:17:07 +0900

    client, server 모듈화

--- src/client_tls.py
+++ src/client_tls.py
@@ -3,44 +3,53 @@ import ssl
 import pprint
 import os
 import random
-def client_run(HOST,PORT,CA_CERT_FILE,CLIENT_CRT,CLIENT_KEY,CLIENT):
- 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((HOST, PORT))
 
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-    context.load_cert_chain(certfile=CLIENT_CRT, keyfile=CLIENT_KEY)
-    context.verify_mode = ssl.CERT_REQUIRED
-    context.load_verify_locations(cafile=CA_CERT_FILE)
-    secure_sock = context.wrap_socket(sock, server_side=False, server_hostname=HOST)
+class Client:
+    def __init__(self,host,port,ca_cert_file,client_crt,client_key):
+        self.host=host
+        self.port=port
+        self.ca_cert_file=ca_cert_file
+        self.client_crt=client_crt
+        self.client_key=client_key
 
-    try:
+    def run(self):
+    
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((self.host, self.port))
 
-        print("Sent client certificate to server")
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+        context.load_cert_chain(certfile=self.client_crt, keyfile=self.client_key)
+        context.verify_mode = ssl.CERT_REQUIRED
+        context.load_verify_locations(cafile=self.ca_cert_file)
+        secure_sock = context.wrap_socket(sock, server_side=False, server_hostname=self.host)
 
-        # 이후 일반 데이터 송수신
-        secure_sock.write(b'hello from client')
-        response = secure_sock.recv(1024)
-        print(f"Received: {response.decode()}")
+        try:
 
-    finally:
-        secure_sock.close()
+            print("Sent client certificate to server")
 
-# Client
+            # 이후 일반 데이터 송수신
+            secure_sock.write(b'hello from client')
+            response = secure_sock.recv(1024)
+            print(f"Received: {response.decode()}")
+
+        finally:
+            secure_sock.close()
+
 if __name__ == '__main__':
 
     CLIENT_PATH="./ssl/client"
     client_list=os.listdir(CLIENT_PATH)
     port_list=[1234,1235,1236]
 
-    for client in client_list:
+    for client_name in client_list:
         HOST = '127.0.0.1'
         PORT =port_list[random.choice([0,1, 2])]
 
         CA_CERT_FILE = "ssl\RootCA\RootCA_with_ServerCA.pem"
         
-        CLIENT_CRT = os.path.join(CLIENT_PATH,client,client+".crt")
-        CLIENT_KEY =  os.path.join(CLIENT_PATH,client,client+".key")
+        CLIENT_CRT = os.path.join(CLIENT_PATH,client_name,client_name+".crt")
+        CLIENT_KEY =  os.path.join(CLIENT_PATH,client_name,client_name+".key")
 
-        client_run(HOST,PORT,CA_CERT_FILE,CLIENT_CRT,CLIENT_KEY,client)
-        print('finish '+client)
+        client=Client(HOST,PORT,CA_CERT_FILE,CLIENT_CRT,CLIENT_KEY)
+        client.run()
+        print('finish '+client_name)
